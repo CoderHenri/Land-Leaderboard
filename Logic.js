1222ea420f894b87c6ad7f856e829a90662fec9d
Henri0812 <p.metzner@gmx.at>
2020-04-20 18:25:31 +0200

    Leaderboards work, though no  names

--- Logic.js
+++ Logic.js
@@ -1,111 +1,31 @@
-function GetLandData() {
-   
-
-    var GenesisPlotOwners = [];
-    var GenLeaders = [];
-  
-    var url = "https://axieinfinity.com/graphql-server/graphql"
-    var col = -30;
-    var row = 30;
-    var i = 0;
-    var y = 0;
-  
-    while (i < 82) {
-      fetch(url, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json",
-        },
-        body: JSON.stringify({
-          operationName : "GetLandDetail",
-          variables: { 
-            row, 
-            col,
-          },
-          query: "query GetLandDetail($col: Int!, $row: Int!) {   land(col: $col, row: $row) {     ...LandDetail     __typename   } }  fragment LandDetail on Land {   realTokenId   owner   landType   row   col   bundles   auction {     ...AxieAuction     __typename   }   offers {     ...OfferDetail     __typename   }   ownerProfile {     name     __typename   }   __typename }  fragment AxieAuction on Auction {   startingPrice   endingPrice   startingTimestamp   endingTimestamp   duration   timeLeft   currentPrice   currentPriceUSD   suggestedPrice   seller   listingIndex   auctionType   __typename }  fragment OfferDetail on Offer {   offerer {     ...ProfileBrief     __typename   }   listingIndex   price   createdAt   __typename }  fragment ProfileBrief on AccountProfile {   accountId   addresses {     ...Addresses     __typename   }   email   name   __typename }  fragment Addresses on NetAddresses {   ethereum   tomo   loom   __typename } "
-        })
-      })
-      .then(function(response) { 
-        return response.json(); 
-      })
-  
-      .then(function(data) {
-  
-        var text = "Invalid";
-        try {       //catches the error if there is either a street or no land owner account
-          text = data.data.land.ownerProfile.name;
-          if(text == null) {
-            text = data.data.land.owner;
-          } 
-  
-          if(text != "Invalid") {
-            GenesisPlotOwners.push(text);
-            
-          } 
-        }
-        catch {
-          text = "Non-Registered Users";
-  
-          GenesisPlotOwners.push(text);
-        }
-        console.log(GenesisPlotOwners);
-        console.log("entire data:", data);
-  
-        if(y == 81) {
-  
-          GenesisPlotOwners.sort();
-  
-          for(d=0; d < 7; d++){     // Deletes the street plots between the Genesis plots
-            const index = GenesisPlotOwners.indexOf("Non-Registered Users");
-            if (index > -1) {
-              GenesisPlotOwners.splice(index, 1);
-            }
-          }
-  
-          var current = null;
-          var cnt = 0;
-          for (var i = 0; i < GenesisPlotOwners.length; i++) {
-              if (GenesisPlotOwners[i] != current) {
-                  if (cnt > 0) {
-  
-                      GenLeaders.push({amount : cnt, owner : current});
-                  }
-                  current = GenesisPlotOwners[i];
-                  cnt = 1;
-              } else {
-                  cnt++;
-              }
-          }
-          if (cnt > 0) {
-              GenLeaders.push({amount : cnt, owner : current});
-          }
-  
-          GenLeaders.sort((a,b) => b.amount - a.amount || a.owner - b.owner);
-  
-          document.getElementById('GList').innerHTML = '<ol id="GL">' + GenLeaders.map(function (genesis) {
-              return '<li>' + String(genesis["amount"]) + " Genesis Plot(s) owned by " + String(genesis["owner"]) + '</li>';
-          }).join('') + '</ol>';
-          console.log(GenLeaders);
-        }
-        console.log(y);
-        y++;
-      });
-      if(row > -30) {
-        row = row - 1;
-      } else if(col < -9) {
-        col = col + 1;
-      }
-      console.log(row, col);
-      i++;
-    }  
-  }
-
 //Testbed
 
-function GetLandData3() {
+function GetLandData() {
  
   var url = "https://axieinfinity.com/graphql-server/graphql"
+  
+  //Total
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+      
+    body: JSON.stringify({
+      "operationName":"GetLandsGrid","variables":{"from":0,"size":100,"sort":"PriceAsc","criteria":{"owner":null,"type":["Genesis"]}},
+      "query":"query GetLandsGrid($from: Int!, $size: Int!, $sort: LandsSortBy!, $criteria: LandsCriteria) {\n  lands(criteria: $criteria, from: $from, size: $size, sort: $sort) {\n    total\n    result {\n      ...LandBriefV2\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment LandBriefV2 on Land {\n  realTokenId\n  owner\n  landType\n  row\n  col\n  auction {\n    currentPrice\n    startingTimestamp\n    currentPriceUSD\n    __typename\n  }\n  __typename\n}\n"})
+    })
+    .then(function(response) { 
+      return response.json(); 
+    })
+  
+    .then(function(data) {
+      var GenesisData = data;
+      LeaderboardMaker(data, 'GList');
+      console.log("entire data:", data);
+  });
+
   
   //Genesis
   fetch(url, {
@@ -125,7 +45,7 @@ function GetLandData3() {
   
     .then(function(data) {
       var GenesisData = data;
-      LeaderboardMaker(GenesisData);
+      LeaderboardMaker(data, 'GList');
       console.log("entire data:", data);
   });
     
@@ -146,6 +66,7 @@ function GetLandData3() {
     })
   
     .then(function(data) { 
+      LeaderboardMaker(data, 'MList');
       console.log("entire data:", data);
   });
  
@@ -166,6 +87,7 @@ function GetLandData3() {
     })
   
     .then(function(data) { 
+      LeaderboardMaker(data, 'AList');
       console.log("entire data:", data);
   });
 
@@ -186,6 +108,7 @@ function GetLandData3() {
     })
   
     .then(function(data) { 
+      LeaderboardMaker(data, 'FList');
       console.log("entire data:", data);
   });
 
@@ -206,6 +129,7 @@ function GetLandData3() {
     })
   
     .then(function(data) { 
+      LeaderboardMaker(data, 'SList');
       console.log("entire data:", data);
   });
 }
@@ -216,7 +140,7 @@ function GetLandData2() {
   var url = "https://axieinfinity.com/graphql-server/graphql"
 
 
-  //Genesis
+  //Total, needs 3 fetch
   fetch(url, {
     method: "POST",
     headers: {
@@ -225,7 +149,7 @@ function GetLandData2() {
     },
       
     body: JSON.stringify({
-      "operationName":"GetLandsGrid","variables":{"from":0,"size":100,"sort":"PriceAsc","criteria":{"owner":null,"type":["Genesis"]}},
+      "operationName":"GetLandsGrid","variables":{"from":0,"size":2000,"sort":"PriceAsc","criteria":{"owner":null,"type":[]}},
       "query":"query GetLandsGrid($from: Int!, $size: Int!, $sort: LandsSortBy!, $criteria: LandsCriteria) {\n  lands(criteria: $criteria, from: $from, size: $size, sort: $sort) {\n    total\n    result {\n      ...LandBriefV2\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment LandBriefV2 on Land {\n  realTokenId\n  owner\n  landType\n  row\n  col\n  auction {\n    currentPrice\n    startingTimestamp\n    currentPriceUSD\n    __typename\n  }\n  __typename\n}\n"})
     })
     .then(function(response) { 
@@ -233,15 +157,17 @@ function GetLandData2() {
     })
   
     .then(function(data) {
-      LeaderboardMaker(data);
+      //LeaderboardMaker(data, 'GList');
       console.log("entire data:", data);
   });
+
+ 
 }
 
 
-function LeaderboardMaker(b){
+function LeaderboardMaker(JSONData, IDList){
 
-  console.log("LeaderboardMaker" + JSON.stringify(b));
+  console.log("LeaderboardMaker" + JSON.stringify(JSONData));
 
   var Owners = [];
   var OwnersLeaderboard = [];
@@ -250,10 +176,10 @@ function LeaderboardMaker(b){
   var LoopNumber = 0;
   var LandText = "empty";
 
-  LandNumber = b.data.lands.total;  //works
+  LandNumber = JSONData.data.lands.total;  //works
 
   while(LoopNumber < LandNumber) {
-    LandText = b.data.lands.result[LoopNumber].owner;
+    LandText = JSONData.data.lands.result[LoopNumber].owner;
     Owners.push(LandText);
     LoopNumber = LoopNumber + 1;
   }
@@ -281,8 +207,8 @@ function LeaderboardMaker(b){
 
   OwnersLeaderboard.sort((a,b) => b.amount - a.amount || a.owner - b.owner);
 
-  document.getElementById('GList').innerHTML = '<ol id="GL">' + OwnersLeaderboard.map(function (genesis) {
-      return '<li>' + String(genesis["amount"]) + " Genesis Plot(s) owned by " + String(genesis["owner"]) + '</li>';
+  document.getElementById(IDList).innerHTML = '<ol class="LL">' + OwnersLeaderboard.map(function (genesis) {
+      return '<li>' + String(genesis["amount"]) + " Plots owned by " + String(genesis["owner"]) + '</li>';
   }).join('') + '</ol>';
   console.log("OwnerLead: " + JSON.stringify(OwnersLeaderboard));
 
