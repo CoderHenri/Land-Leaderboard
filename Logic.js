a868e690ed6edb3aa199db58bff8d400ab494d69
Henri0812 <p.metzner@gmx.at>
2020-04-22 00:02:45 +0200

    Deleted Testbed Code

--- Logic.js
+++ Logic.js
@@ -15,32 +15,6 @@ function GetLandData(Reihenfolge) {
   } else if(Reihenfolge === "Savannah") {
     GetSavannah(url, Reihenfolge);
   } 
-
-
-  /*not working
-  //Total
-  fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Accept": "application/json",
-    },
-      
-    body: JSON.stringify({
-      "operationName":"GetLandsGrid","variables":{"from":0,"size":100,"sort":"PriceAsc","criteria":{"owner":null,"type":["Genesis"]}},
-      "query":"query GetLandsGrid($from: Int!, $size: Int!, $sort: LandsSortBy!, $criteria: LandsCriteria) {\n  lands(criteria: $criteria, from: $from, size: $size, sort: $sort) {\n    total\n    result {\n      ...LandBriefV2\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment LandBriefV2 on Land {\n  realTokenId\n  owner\n  landType\n  row\n  col\n  auction {\n    currentPrice\n    startingTimestamp\n    currentPriceUSD\n    __typename\n  }\n  __typename\n}\n"})
-    })
-    .then(function(response) { 
-      return response.json(); 
-    })
-  
-    .then(function(data) {
-      LeaderboardMaker(data, 'GList');
-      console.log("entire data:", data);
-  });
-*/
-
-
 }
 
 
@@ -157,39 +131,6 @@ function GetSavannah(url, Reihenfolge){
       LeaderboardMaker(data, 'SList', Reihenfolge);
       console.log("entire data:", data);
   });
-}
-
-
-
-//Testbed
-
-function GetLandData2() {
- 
-  var url = "https://axieinfinity.com/graphql-server/graphql"
-
-
-  //Total, needs 3 fetch
-  fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Accept": "application/json",
-    },
-      
-    body: JSON.stringify({
-      "operationName":"GetLandsGrid","variables":{"from":0,"size":100,"sort":"PriceAsc","criteria":{"owner":null,"type":["Genesis"]}},
-      "query":"query GetLandsGrid($from: Int!, $size: Int!, $sort: LandsSortBy!, $criteria: LandsCriteria) {\n  lands(criteria: $criteria, from: $from, size: $size, sort: $sort) {\n    total\n    result {\n      ...LandBriefV2\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment LandBriefV2 on Land {\n  realTokenId\n  owner\n  landType\n  row\n  col\n  auction {\n    currentPrice\n    startingTimestamp\n    currentPriceUSD\n    __typename\n  }\n  __typename\n}\n"})
-    })
-    .then(function(response) { 
-      return response.json(); 
-    })
-  
-    .then(function(data) {
-      LeaderboardMaker(data, 'GList');
-      console.log("entire data:", data);
-  });
-
- 
 }
 
 
